76746ce94408c22c18a5f7e127ed68a868b42e1e
hooriya <parih3080@gmail.com>
2024-04-29 17:59:05 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+// // QUESTION 67:
+// function mixedTypes(val1:number,val2:string){
+//     return val1+ Number(val2);
+// }
+// let answer=mixedTypes(10,"5");
+// console.log(answer);
+
+
+
+
+
+
+
+
+
+
+
+// // QUESTION 68:
+// function multiplyingDecimal(num1:number,num2:number){
+//     return Math.round((num1*num2)*100)/100;
+// }
+// let answer2=multiplyingDecimal(1.3,3.2);
+// console.log(answer2);
+
+
+
+
+
+
+// QUESTION 69:
+function divindindAndReminder(divident:number,divisor:number)
+:{quotient:number,reminder:number}
+{
+ let quotient=Math.floor(divident/divisor);
+ let reminder=divident%divisor;
+ return {quotient,reminder};
+}
+console.log(divindindAndReminder(2,7));
